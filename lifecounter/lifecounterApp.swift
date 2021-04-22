c88187a6f9b565e792d2d4a479f1fa0c3c70d58d
Vishank Rughwani <vishankr@uw.edu>
2021-04-21 20:11:49 -0700

    Initial Commit

---
+++ lifecounter/lifecounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  lifecounterApp.swift
+//  lifecounter
+//
+//  Created by Vishank Rughwani on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct lifecounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
